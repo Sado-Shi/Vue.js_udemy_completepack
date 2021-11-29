70251ec0d9096fdea4fd1de7088bfa1d9a02fc03
Sado-Shi <derasado04@gmail.com>
2021-11-29 14:27:14 +0900

    single component, local, global

--- udemy-vue.js/src/main.js
+++ udemy-vue.js/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
+import LikeNumber from './Components/LikeNumber.vue';
 
 Vue.config.productionTip = false
+Vue.component('LikeNumber', LikeNumber);
 
 new Vue({
+  el: "#app",
   render: h => h(App)
-}).$mount('#app');
+});
